723054d3d7ff229d7a892a383d54f54088010cec
cogg <adam@cogg.rocks>
2024-12-03 22:02:36 -0700

    runner branch

---
+++ bench.rs
@@ -0,0 +1,23 @@
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+
+use solution::day3::{part1, part2};
+
+const ANSWER_1: i64 = 190604937;
+const ANSWER_2: i64 = 82857512;
+
+fn bench_part1(c: &mut Criterion) {
+    let input = include_str!("input.txt");
+    c.bench_function("part1", |b| {
+        b.iter(|| assert_eq!(part1(black_box(input)), ANSWER_1))
+    });
+}
+
+fn bench_part2(c: &mut Criterion) {
+    let input = include_str!("input.txt");
+    c.bench_function("part2", |b| {
+        b.iter(|| assert_eq!(part2(black_box(input)), ANSWER_2))
+    });
+}
+
+criterion_group!(benches, bench_part1, bench_part2);
+criterion_main!(benches);
